32b7b881fd07665cd4fdff6c089c0f2c64b59cfe
bendoflex <joanzaf@lilo.org>
2020-09-07 00:53:21 +0200

    Read Process OK with SetInterVal and Main FLow begins

---
+++ header.php
@@ -0,0 +1,28 @@
+<!--PHP dynamic style TODO-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href="style.css"/>
+    <script type="text/javascript" src="koze.js"></script>
+    <title>Kozé</title>
+</head>
+<body>
+
+<header>
+    <div id="logo_container">
+        <h1>Kozé</h1>
+    </div>
+    <nav>
+        <ul id="menu">
+            <a href="index.php">
+                <li>Home</li>
+            </a>
+            <li>About Us</li>
+            <a href="demo.php"><li>Test It</li></a>
+            <li>Download The App</li>
+            <li>LargTonKozé</li>
+        </ul>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php require("header.php"); ?>
+
+ <div id="statement">
+         <h1>Wanna Learn Something and enjoy a lot about this?</h1>
+
+         <p>Kozé : The App that help you to conqueer the world with words</p>    
+ </div>
+
+    
+</body>
+</html>
